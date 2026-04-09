023a971b5485f574708fd6051aa8f6cef7222bee
Vinicius Hissa <166537440+viniciushissa@users.noreply.github.com>
2026-04-09 09:01:21 -0300

    feat: database has been created (#8)
    * feat: database has been created
    
    * feat: database has been created #2
    
    * .gitignore modified
    
    * venv removed
    
    * fix: gitignore

---
+++ database.py
@@ -0,0 +1,34 @@
+import psycopg2
+import os
+
+conn = psycopg2.connect(
+    host='localhost',
+    port=5435,
+    dbname=os.getenv('POSTGRES_DB'),
+    user=os.getenv('POSTGRES_USER'),
+    password=os.getenv('POSTGRES_PASSWORD')
+    )
+
+cur = conn.cursor()
+
+cur.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                phone VARCHAR(20) PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                nickname VARCHAR(100) NOT NULL,
+                password VARCHAR(100) NOT NULL
+            );
+            CREATE TABLE IF NOT EXISTS messages (
+                id SERIAL PRIMARY KEY,
+                sender_phone VARCHAR(20) NOT NULL,
+                receiver_phone VARCHAR(20) NOT NULL,
+                content TEXT NOT NULL,
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (sender_phone) REFERENCES users(phone),
+                FOREIGN KEY (receiver_phone) REFERENCES users(phone)
+            );
+''')
+
+conn.commit()
+cur.close()
+conn.close()
