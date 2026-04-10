ff0c1651314b6dd449f79be66306210e21704204
Vinicius Hissa <166537440+viniciushissa@users.noreply.github.com>
2026-04-10 10:38:23 -0300

    Feat/database (#9)
    * feat: database has been created
    
    * feat: database has been created #2
    
    * .gitignore modified
    
    * venv removed
    
    * fix: gitignore
    
    * fix: load env to connect to db properly
    
    * fix: docker compose port and hardcoded removed
    
    ---------
    
    Co-authored-by: Juan Cristo <jziignerdev@gmail.com>

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 import psycopg2
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 conn = psycopg2.connect(
     host='localhost',
-    port=5435,
+    port=os.getenv('POSTGRES_PORT'),
     dbname=os.getenv('POSTGRES_DB'),
     user=os.getenv('POSTGRES_USER'),
     password=os.getenv('POSTGRES_PASSWORD')
@@ -18,12 +21,16 @@ cur.execute('''
                 nickname VARCHAR(100) NOT NULL,
                 password VARCHAR(100) NOT NULL
             );
+''')
+
+cur.execute('''
             CREATE TABLE IF NOT EXISTS messages (
                 id SERIAL PRIMARY KEY,
                 sender_phone VARCHAR(20) NOT NULL,
                 receiver_phone VARCHAR(20) NOT NULL,
                 content TEXT NOT NULL,
                 timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                status VARCHAR(20) DEFAULT 'sent',
                 FOREIGN KEY (sender_phone) REFERENCES users(phone),
                 FOREIGN KEY (receiver_phone) REFERENCES users(phone)
             );
