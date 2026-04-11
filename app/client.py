6d8c5b4da575dbcd0bcb39389fda15dc709dfb09
viniciushissa <vini16.hissa@gmail.com>
2026-04-11 09:28:05 -0300

    fix: checks.py has been renamed as utils.py | client.py now using websockets | websockets package has been included in requirements.txt

--- app/client.py
+++ app/client.py
@@ -1,79 +1,79 @@
-import socket
-import threading
+import asyncio
+from websockets.asyncio.client import connect
 import json
-from checks import phone_check, name_check, nickname_check, password_check
+from utils import URI, phone_check, name_check, nickname_check, password_check
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    client.connect(('127.1.1.1', 5000))
-except Exception as e:
-    print(f"Erro ao conectar ao servidor: {e}")
+async def main():
+    try:
+        async with connect(URI) as websocket:
+            while True:
+                option = input("1-Cadastro\n2-Login\n:")
+                
+                if option == "1":
+                    while True:
+                        phone = input("Digite seu número de telefone: ")
+                        if phone_check(phone):
+                            break
 
-while True:
-    option = input("1-Cadastro\n2-Login\n:")
-    if option == "1":
-        while True:
-            phone = input("Digite seu número de telefone: ")
-            if phone_check(phone):
-                break
+                    while True:
+                        name = input("Digite seu nome: ")
+                        if name_check(name):
+                            break
 
-        while True:
-            name = input("Digite seu nome: ")
-            if name_check(name):
-                break
+                    while True:
+                        nickname = input("Digite seu apelido: ")
+                        if nickname_check(nickname):
+                            break
 
-        while True:
-            nickname = input("Digite seu apelido: ")
-            if nickname_check(nickname):
-                break
+                    while True:
+                        password = input("Digite sua senha: ")
+                        if password_check(password):
+                            break
 
-        while True:
-            password = input("Digite sua senha: ")
-            if password_check(password):
-                break
-        
-        print("Realizando cadastro...")
-        client.send(json.dumps({
-            "type": "register",
-            "phone": phone,
-            "name": name,
-            "nickname": nickname,
-            "password": password
-        }).encode())
-        
-        response = client.recv(1024)
-        data = json.loads(response.decode())
+                    print("Realizando cadastro...")
+                    await websocket.send(json.dumps({
+                        "type": "register",
+                        "phone": phone,
+                        "name": name,
+                        "nickname": nickname,
+                        "password": password
+                    }))
 
-        if data["register_status"] == "success":
-            ...
+                    response = await websocket.recv()
+                    data = json.loads(response)
 
-        elif data["register_status"] == "error":
-            ...
-        
-        
-    elif option == "2":
-        while True:
-            phone = input("Digite seu número de telefone: ")
-            if phone_check(phone):
-                break
+                    if data["register_status"] == "success":
+                        ...
+                    elif data["register_status"] == "error":
+                        ...
 
-        while True:
-            password = input("Digite sua senha: ")
-            if password_check(password):
-                break
-        
-        print("Efetuando login...")
-        client.send(json.dumps({
-            "type": "login",
-            "phone": phone,
-            "password": password
-        }).encode())
-        
-        response = client.recv(1024)
-        data = json.loads(response.decode())
+                elif option == "2":
+                    while True:
+                        phone = input("Digite seu número de telefone: ")
+                        if phone_check(phone):
+                            break
 
-        if data["login_status"] == "success":
-            ...
+                    while True:
+                        password = input("Digite sua senha: ")
+                        if password_check(password):
+                            break
 
-        elif data["login_status"] == "error":
-            ...
+                    print("Efetuando login...")
+                    await websocket.send(json.dumps({
+                        "type": "login",
+                        "phone": phone,
+                        "password": password
+                    }))
+
+                    response = await websocket.recv()
+                    data = json.loads(response)
+
+                    if data["login_status"] == "success":
+                        ...
+                    elif data["login_status"] == "error":
+                        ...
+
+    except Exception as e:
+        print(f"Erro ao conectar ao servidor: {e}")
+
+asyncio.run(main())
