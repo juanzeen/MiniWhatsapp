09636eef33ec16d6e24ec8be53130e6b9e12aed5
viniciushissa <vini16.hissa@gmail.com>
2026-04-11 11:30:07 -0300

    feat: register and login have been created. feat stopped in home options

--- app/client.py
+++ app/client.py
@@ -7,7 +7,7 @@ async def main():
     try:
         async with connect(URI) as websocket:
             while True:
-                option = input("1-Cadastro\n2-Login\n:")
+                option = input("1-Cadastro\n2-Login\n3-Sair do aplicativo\n:")
                 
                 if option == "1":
                     while True:
@@ -29,23 +29,26 @@ async def main():
                         password = input("Digite sua senha: ")
                         if password_check(password):
                             break
+                    try:        
+                        print("Realizando cadastro...")
+                        await websocket.send(json.dumps({
+                            "type": "register",
+                            "phone": phone,
+                            "name": name,
+                            "nickname": nickname,
+                            "password": password
+                        }))
 
-                    print("Realizando cadastro...")
-                    await websocket.send(json.dumps({
-                        "type": "register",
-                        "phone": phone,
-                        "name": name,
-                        "nickname": nickname,
-                        "password": password
-                    }))
+                        response = await websocket.recv()
+                        data = json.loads(response)
 
-                    response = await websocket.recv()
-                    data = json.loads(response)
+                        if data["register_status"] == "success":
+                            print("Cadastro criado com sucesso! Realize o Login.\n")
+                        elif data["register_status"] == "error":
+                            print(f"Erro ao realizar o cadastro!\nMotivo do erro: {data["reason"]}\nTente novamente.\n")
 
-                    if data["register_status"] == "success":
-                        ...
-                    elif data["register_status"] == "error":
-                        ...
+                    except Exception as e:
+                        print(f"Erro ao enviar dados de cadastro ao servidor: {e}")
 
                 elif option == "2":
                     while True:
@@ -57,21 +60,66 @@ async def main():
                         password = input("Digite sua senha: ")
                         if password_check(password):
                             break
+                    
+                    try:
+                        print("Efetuando login...")
+                        await websocket.send(json.dumps({
+                            "type": "login",
+                            "phone": phone,
+                            "password": password
+                        }))
 
-                    print("Efetuando login...")
-                    await websocket.send(json.dumps({
-                        "type": "login",
-                        "phone": phone,
-                        "password": password
-                    }))
+                        response = await websocket.recv()
+                        data = json.loads(response)
 
-                    response = await websocket.recv()
-                    data = json.loads(response)
+                        if data["login_status"] == "success":
+                            print("Login efetuado com sucesso!\n")
+                            while True:
+                                home_options = input("1-Contatos\n2-Adicionar novo contato\n3-Logout\n:")
+                                if home_options == "1":
+                                    ...
+                                elif home_options == "2":
+                                    new_contact = input("Digite o número do seu novo contato: ")
+                                    message = input(f"\nNovo contato: {new_contact}\nDigite a primeira mensagem: ")
+                                    # só poderá adicionar um novo contato se mandar uma mensagem, 
+                                    # assim iniciando um novo histórico
+                                    try:
+                                        await websocket.send(json.dumps({
+                                            "type": "message",
+                                            "sender_phone": phone,
+                                            "receiver_phone": new_contact,
+                                            "content": message
+                                        }))
 
-                    if data["login_status"] == "success":
-                        ...
-                    elif data["login_status"] == "error":
-                        ...
+                                        response = await websocket.recv()
+                                        data = json.loads(response)
+
+                                        if data["status"] == "sent":
+                                            print("Contato adicionado!\n")
+                                        else:
+                                            print("Erro ao adicionar novo contato! Tente novamente.")
+
+                                    except Exception as e:
+                                        print("Erro ao enviar mensagem ao novo contato: {e}")
+
+                                elif home_options == "3":
+                                    print("Efetuando Logout...\n")
+                                    break
+                                else: 
+                                    print("Digite uma opção correta!\n")
+
+                        elif data["login_status"] == "error":
+                            print(f"Erro ao realizar o login!\nMotivo do erro: {data["reason"]}\nTente novamente.\n")
+
+                    except Exception as e:
+                        print(f"Erro ao enviar dados de login ao servidor: {e}")
+
+                elif option == "3":
+                    print("Saindo do aplicativo...\n")
+                    break
+
+                else:
+                    print("Digite uma opção correta!\n")
 
     except Exception as e:
         print(f"Erro ao conectar ao servidor: {e}")
--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,7 @@ URI = "ws://127.1.1.1:5000"
 
 def phone_check(phone):
     if len(phone) > 20:
-            print("Número de Telefone muito grande (tam max: 20)\n")
+        print("Número de Telefone muito grande (tam max: 20)\n")
     elif len(phone) < 11:
         print("Número de Telefone muito pequeno (tam min: 11)\n")
     else:
@@ -31,7 +31,7 @@ def nickname_check(nickname):
 
 def password_check(password):
     if len(password) > 100:
-            print("Senha muito grande (tam max: 100)\n")
+        print("Senha muito grande (tam max: 100)\n")
     elif len(password) < 8:
         print("Senha muito pequena (tam min: 8)\n")
     else:
